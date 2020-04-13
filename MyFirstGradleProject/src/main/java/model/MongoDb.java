48a9da86e8a2d44c5bc1cef72ef7b1b7962cf50e
ebazman <63226359+ebazman@users.noreply.github.com>
2020-04-13 16:27:49 +0300

    Create MongoDb.java
    MongoDb as a model is created.

---
+++ MyFirstGradleProject/src/main/java/model/MongoDb.java
@@ -0,0 +1,40 @@
+package model;
+
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.MongoClient;
+
+public class MongoDb {
+	private MongoClient mongoClient;
+	private DB mongoDatabase;
+	private DBCollection mongoCollection;
+	
+	public void connectMongoDb() {
+		try {
+			mongoClient = new MongoClient("localhost", 27017);
+		}
+		catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void setMongoDatabase(String databaseName) {
+		mongoDatabase = mongoClient.getDB(databaseName);
+	}
+	
+	public void setMognoCollection(String collectionName) {
+		mongoCollection = mongoDatabase.getCollection(collectionName);
+	}
+	
+	public DB getMongoDatabase() {
+		return this.mongoDatabase;
+	}
+	
+	public DBCollection getMongoCollection() {
+		return this.mongoCollection;
+	}
+}
